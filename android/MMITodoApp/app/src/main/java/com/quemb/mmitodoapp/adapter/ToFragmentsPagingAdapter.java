b1843a3b41f71d7d4c20e748e3134df139b94ff5
Toni Möckel <tonimockel@Tonis-MBP.fritz.box>
2016-06-28 21:45:55 +0200

    Tab Host mit Tabs

--- android/MMITodoApp/app/src/main/java/com/quemb/mmitodoapp/adapter/ToFragmentsPagingAdapter.java
+++ android/MMITodoApp/app/src/main/java/com/quemb/mmitodoapp/adapter/ToFragmentsPagingAdapter.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
 import java.util.ArrayList;
 
